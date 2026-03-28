afe9b47576c4d8e7955adf782bb5b2cde1ad78a6
Ben Link <link.ben@gmail.com>
2026-03-27 20:18:55 -0400

    Adds first models

---
+++ src/app/models/serenity.py
@@ -0,0 +1,19 @@
+'''The Class for Serenity'''
+
+class Serenity:
+    '''
+    This class defines the ship's attributes...
+    - The carrying capacity of various supplies
+      - Fuel
+      - Spare Parts
+      - Food
+      - Battery power
+
+    - The current hull damage
+    - The threat level with the Alliance
+    - The threat level with Reavers
+    - The threat level with Gangsters
+    '''
+    def __init__(self):
+        
+        pass
